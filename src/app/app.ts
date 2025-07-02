8f558863b9188ecb6cab3947b333e787dd464227
yashwingahlawat <yashwingahlawat@gmail.com>
2025-07-03 00:30:56 +0530

    ok

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'angular-tut';
+  protected title = 'yashwin';
+  factorial(val:number):number {
+    if(val<=1)return 1;
+    return val*this.factorial(val-1)
+  }
 }
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core"
+
+@Component({
+    selector:'app-profle',
+    template:`<h1>Profile Component</h1>`,
+    styles:`h1{color:red}`
+})
+
+export class Profile{
+
+}
